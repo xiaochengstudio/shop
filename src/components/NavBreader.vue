a20a0942806a19e424644330569fb2e50bdd243e
chengxin <feijinshuliu@163.com>
2018-04-09 13:35:25 +0800

    拆分组件

---
+++ src/components/NavBreader.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+      <div class="nav-breadcrumb-wrap">
+        <div class="container">
+          <nav class="nav-breadcrumb">
+            <a href="/">主页</a>
+            <span>商品列表</span>
+          </nav>
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+  import '../assets/css/nav-bread.css';
+    export default {
+        name: "nav-breader"
+    }
+</script>
+
+<style scoped>
+
+</style>
